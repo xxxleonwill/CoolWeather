bf85c4430c82c0ae8981141a37371ac74c6ddb5d
xxxleonwill <1391755128@qq.com>
2020-04-11 07:12:12 +0000

    Add fragment.

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -4,8 +4,9 @@ import org.litepal.crud.LitePalSupport;
 
 public class County extends LitePalSupport {
     private int id;
-
     private String countyName;
+    private String weatherId;
+    private int cityId;
 
     public int getId() {
         return id;
@@ -38,9 +39,4 @@ public class County extends LitePalSupport {
     public void setCityId(int cityId) {
         this.cityId = cityId;
     }
-
-    private String weatherId;
-
-    private int cityId;
-
 }
